f4cea3dd566634f4bf947b9af5b2165f3a46c04f
stlad <82213506+stlad@users.noreply.github.com>
2021-05-30 16:06:25 +0500

    добавлен тестовый этап и возможность его редактировать

---
+++ DeepDarkSpace/Assets/Scripts/TestScripts/AnswerButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public  bool IsCorrect;
+    public bool IsClicked = false;
+    public GameObject CurrentCond;
+    
+    void Start()
+    {
+        var button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(() => 
+        {
+            IsClicked = true;
+            if (IsCorrect)
+                CurrentCond.GetComponent<CurrentCondition>().SetLevelCompleted(); 
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClicked)
+        {
+            if (IsCorrect)
+                gameObject.GetComponent<Image>().color = Color.green;
+            else
+                gameObject.GetComponent<Image>().color = Color.red;
+        }
+    }
+    
+    
+}
---
+++ DeepDarkSpace/Assets/Scripts/TestScripts/TestTextParser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTextParser : MonoBehaviour
+{
+    public GameObject Question;
+    public List<GameObject> Answers;
+    void Start()
+    {
+        var text = (TextAsset)Resources.Load($"{CurrentCondition.AgeType}_{CurrentCondition.LevelType}");
+        var allStrings = text.text.Split('_');
+        var str = allStrings.Where(n => n.Contains($"[{CurrentCondition.LevelId}]")).FirstOrDefault();
+        var levelPrefixCount = 2 + CurrentCondition.LevelId.ToString().Count();
+
+        var ansStr = str.Split('\n');
+        Question.GetComponent<Text>().text = ansStr.First().Remove(0, levelPrefixCount);
+
+        //Answers.ForEach((n,i)=>i+1)
+
+        for(int i = 0; i<Answers.Count;i++)
+        {
+            if(ansStr[i + 1][0]=='*')
+            {
+                Answers[i].GetComponent<AnswerButton>().IsCorrect = true;
+            }
+            Answers[i].transform.Find("Text").GetComponent<Text>().text = ansStr[i+1].Trim('*');
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
