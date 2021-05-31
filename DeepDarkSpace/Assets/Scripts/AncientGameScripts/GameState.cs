97e4b0b782622408a8254d42e4addda22fb45244
stlad <82213506+stlad@users.noreply.github.com>
2021-05-31 15:35:16 +0500

    выделение звезд для мини игры

---
+++ DeepDarkSpace/Assets/Scripts/AncientGameScripts/GameState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    //public LimitedSizeLIst<GameObject> ActiveStars = new LimitedSizeLIst<GameObject>(2);
+    public  List<GameObject> ActiveStars = new List<GameObject>();
+    public int Limit;
+
+    public void Add(GameObject star)
+    {
+        if (ActiveStars.Count == Limit)
+        {
+            ActiveStars[0].GetComponent<StarController>().IsActive = false;
+            ActiveStars.RemoveAt(0);
+        }
+        ActiveStars.Add(star);
+        star.GetComponent<StarController>().IsActive = true ;
+    }
+
+    public void Remove(GameObject star)
+    {
+        ActiveStars.Remove(star);
+        star.GetComponent<StarController>().IsActive = false;
+    }
+}
---
+++ DeepDarkSpace/Assets/Scripts/AncientGameScripts/StarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    public bool IsActive = false;
+    private void OnMouseDown()
+    {
+        IsActive = !IsActive;
+        var state = gameObject.GetComponentInParent<GameState>();
+        if (IsActive) state.Add(gameObject);
+        else state.Remove(gameObject);
+        Debug.Log("!!!");
+    }
+
+    private void Update()
+    {
+        if (IsActive) gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        else gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
