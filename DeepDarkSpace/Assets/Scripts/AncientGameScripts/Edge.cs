af26d71dbf1b8d63e9675d5d0185bc7fe9eb3026
stlad <82213506+stlad@users.noreply.github.com>
2021-05-31 16:19:30 +0500

    рисование отрезвов между звездами

---
+++ DeepDarkSpace/Assets/Scripts/AncientGameScripts/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour
+{
+    public GameObject From = new GameObject();
+    public GameObject To = new GameObject();
+
+    void Start()
+    {
+        var line = gameObject.GetComponent<LineRenderer>();
+        var pos = new List<Vector3>();
+        pos.Add(From.transform.position);
+        pos.Add(To.transform.position);
+        line.SetPositions(pos.ToArray());
+        Instantiate(line);
+        var state = gameObject.GetComponentInParent<GameState>();
+        state.GetComponent<GameState>().Edges.Add(gameObject);
+    }
+}
--- DeepDarkSpace/Assets/Scripts/AncientGameScripts/GameState.cs
+++ DeepDarkSpace/Assets/Scripts/AncientGameScripts/GameState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,10 @@ public class GameState : MonoBehaviour
 {
     //public LimitedSizeLIst<GameObject> ActiveStars = new LimitedSizeLIst<GameObject>(2);
     public  List<GameObject> ActiveStars = new List<GameObject>();
-    public int Limit;
+    public List<GameObject> Edges = new List<GameObject>(); 
+    public int Limit = 2;
+    public GameObject AddEdgeButton;
+    public GameObject EdgePrefab;
 
     public void Add(GameObject star)
     {
@@ -17,11 +21,26 @@ public class GameState : MonoBehaviour
         }
         ActiveStars.Add(star);
         star.GetComponent<StarController>().IsActive = true ;
+
+        if (ActiveStars.Count == Limit) AddEdgeButton.SetActive(true);
+        else AddEdgeButton.SetActive(false);
     }
 
     public void Remove(GameObject star)
     {
         ActiveStars.Remove(star);
         star.GetComponent<StarController>().IsActive = false;
+        AddEdgeButton.SetActive(false);
+    }
+
+    public void InstantiateEdgeWithActiveStars()
+    {
+        var edge = Instantiate(EdgePrefab);
+        var pos = new List<Vector3>();
+        pos.Add(ActiveStars[0].transform.position);
+        pos.Add(ActiveStars[1].transform.position);
+        edge.GetComponent<LineRenderer>().SetPositions(pos.ToArray());
+
+        Edges.Add(edge);
     }
 }
