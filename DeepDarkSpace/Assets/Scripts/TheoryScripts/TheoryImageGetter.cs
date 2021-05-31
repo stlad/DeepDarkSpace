68d0f72b805b7cbee466b5d96867aee8d0635cbf
stlad <82213506+stlad@users.noreply.github.com>
2021-05-31 12:16:30 +0500

    извлечение изображений для теоретических этапов

---
+++ DeepDarkSpace/Assets/Scripts/TheoryScripts/TheoryImageGetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheoryImageGetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite image = Resources.Load<Sprite>($"{CurrentCondition.AgeType}_{CurrentCondition.LevelType}_{CurrentCondition.LevelId}");
+        gameObject.GetComponent<Image>().sprite = image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
